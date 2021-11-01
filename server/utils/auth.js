2cfa0eaed7f1d2ea0d1f0fa814fd7298fdab00ec
Pasgen-Manley <pasgen95@gmail.com>
2021-11-01 21:11:15 +0800

    jwt auth added server side, dependicies added in package.json

---
+++ server/utils/auth.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const secret = '4421694204421Jeff';
+const expiration = '2h';
+
+module.exports = {
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
