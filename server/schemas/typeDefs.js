87a5c3229bb466a9c7ce82096de24008bc617774
Pasgen-Manley <pasgen95@gmail.com>
2021-11-01 19:01:42 +0800

    typedefs completed for project model

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Project {
+    _id: ID!
+    name: String!
+    description: String!
+    releaseDate: Date!
+    website: String
+    twitter: String
+    discord: String
+    openSea: String
+  }
+
+  type Query {
+    projects: [Project]
+  }
+
+  type Mutation {
+    addProject(
+      name: String!, description: String!, releaseDate: Date!, website: String, twitter: String, discord: String, openSea: String
+    ): Project
+  }
+`;
+
+module.exports = typeDefs;
