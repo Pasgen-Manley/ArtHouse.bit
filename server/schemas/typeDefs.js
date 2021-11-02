a708562125fb51ef06f20520da4871d95e19cdc6
Pasgen-Manley <pasgen95@gmail.com>
2021-11-02 18:57:06 +0800

    added user to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,13 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+  }
+
   type Project {
     _id: ID!
     projectName: String!
@@ -12,11 +19,20 @@ const typeDefs = gql`
     openSea: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    users: [User]
+    user(username: String!): User
     projects: [Project]
   }
 
   type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
     addProject(
       projectName: String!, description: String!, releaseDate: Date!, website: String, twitter: String, discord: String, openSea: String
     ): Project
