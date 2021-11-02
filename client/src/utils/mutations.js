a06a4cb0ee749d3e4496ec2d3ec71a0903cc37d5
Pasgen-Manley <pasgen95@gmail.com>
2021-11-02 19:12:28 +0800

    Added a login and add user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,29 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const ADD_PROJECT = gql`
   mutation addProject($projectName: String!, $description: String!, $releaseDate: Date!, $website: String, $twitter: String, $discord: String, $openSea: String) {
     addProject(projectName: $projectName, description: $description, releaseDate: $releaseDate, website: $website, twitter: $twitter, discord: $discord, openSea: $openSea) {
