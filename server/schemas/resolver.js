163cd4c8130f4b11c3124a9f8253d49d2f9162e0
Pasgen-Manley <pasgen95@gmail.com>
2021-11-02 19:04:39 +0800

    added user query, adduser and login mutations

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -2,12 +2,40 @@ const { Project } = require('../models');
 
 const resolvers = {
   Query: {
+    users: async () => {
+      return User.find({});
+    },
+    user: async (parent, {username}) => {
+      return User.findOne({ username });
+    },
     projects: async () => {
       return Project.find({});
     },
   },
 
   Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this username or password!');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('No user found with this username or password!');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
     addProject: async (parent, { projectName, description, releaseDate, website, twitter, discord, openSea }) => {
       return Project.create({ projectName, description, releaseDate, website, twitter, discord, openSea });
     },
