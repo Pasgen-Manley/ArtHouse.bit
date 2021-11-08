4f151928b306064ba9d75f1b20b9e944fcc9759a
Pasgen-Manley <pasgen95@gmail.com>
2021-11-08 15:07:16 +0800

    cleaned up code

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,6 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-
-//import Signup from './pages/Signup';
-//import Login from './pages/Login';
-import Header from './components/Header';
 import NavBar from './components/NavBar';
 import ProjectForm from './components/ProjectForm';
 import Login from './pages/Login';
